180cb1c2e292e86a9ea4bdba0435e3b5bc22010f
Andrew Kihs <ajkihs@gmail.com>
2021-04-07 17:08:28 -0400

    AFinish testing for UserController

--- aspiration_app/spec/controllers/users_controller_spec.rb
+++ aspiration_app/spec/controllers/users_controller_spec.rb
@@ -6,27 +6,41 @@ RSpec.describe UsersController, type: :controller do
     
     it "should get the :new form to sign up a user" do 
       get :new
-      expect(response).to render(:new)
+      expect(response).to render_template(:new)
     end
   end
 
   describe '#create' do 
-    let(:user) { FactoryBot.create(:user) }
+
     
     context 'valid params' do 
-      it 'should save user to db' do 
-        post :create, user: { username: "babs", password: "password"}
-        expect(User.last.username).to eq("babs")
-      end
 
-      it 'should redirect to index? or main page' do 
-        
-      end
+        before(:each) do
+            post :create, params: { user: {username: "JerrySeinfeld", password: "password"}}
+        end
+
+        it 'should save user to db' do 
+            expect(User.last.username).to eq("JerrySeinfeld")
+        end
+        # TODO login!(@user)
+
+        it 'should redirect to :show' do 
+            expect(response).to redirect_to(user_url(User.last))
+        end
     end
 
     context 'invalid params' do 
-      # should render the :new page
-      # should add errors to flash
+        before(:each) do
+            post :create, params: { user: {username: "JerrySeinfeld", password: "short"}}
+        end
+
+        it 'should render the :new page' do
+            expect(response).to render_template(:new)
+        end
+        
+        it 'should add errors to flash' do
+            expect(flash[:errors]).to be_present
+        end
     end
   end
   
