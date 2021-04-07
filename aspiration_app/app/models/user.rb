fb220c1ce254ea547f3560b91797ac851b20343b
fdiengott <freddiengott@gmail.com>
2021-04-07 15:16:37 -0400

    Add user.rb validations and password getter/setter

--- aspiration_app/app/models/user.rb
+++ aspiration_app/app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :session_token, uniqueness: true, presence: true
+
+
+  def password=(pw)
+    self.password_digest = BCrypt::Password.create(pw)
+    @password = pw
+  end
+
+  def password
+    @password
+  end
     
 end
