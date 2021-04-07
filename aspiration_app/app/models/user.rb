93bb79a380de663b6de4000e03e6138270eb32ae
Andrew Kihs <ajkihs@gmail.com>
2021-04-07 15:38:34 -0400

    Add several tests/methods for user model

--- aspiration_app/app/models/user.rb
+++ aspiration_app/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6, allow_nil: true }
   validates :session_token, uniqueness: true, presence: true
 
+  after_initialize :ensure_session_token
 
   def password=(pw)
     self.password_digest = BCrypt::Password.create(pw)
@@ -13,5 +14,13 @@ class User < ApplicationRecord
   def password
     @password
   end
-    
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def is_password?(pw)
+    pw_obj = BCrypt::Password.new(self.password_digest)
+    pw_obj.is_password?(pw)
+  end
 end
--- aspiration_app/spec/models/user_spec.rb
+++ aspiration_app/spec/models/user_spec.rb
@@ -6,18 +6,31 @@ RSpec.describe User, type: :model do
         before(:each) do 
             create(:user)
         end
-        # subject(:test_user) { FactoryBot.build(:user)}
-        # subject(:test_user) { User.create!(username: "test_user", password: "password")}
+        # subject(:user) { FactoryBot.build(:user)}
+        # subject(:user) { User.create!(username: "test_user", password: "password")}
 
         it { should validate_presence_of(:username)}
         it { should validate_uniqueness_of(:username)}
-        it { should validate_presence_of(:password)}
         it { should validate_length_of(:password).is_at_least(6)}
         it { should validate_presence_of(:password_digest)}
         it { should validate_presence_of(:session_token)}
     end
 
-    describe 'class and instance method' do
+    describe "#is_password? test" do
+       let(:user) {create(:user)}
+       
+        context 'with valid params' do
+            it 'should return true' do 
+                expect(user.is_password?('password')).to be true
+            end
+        end
 
+        context 'with invalid params' do
+            it 'should return false' do 
+                expect(user.is_password?('not the right pw')).to be false
+            end
+        end
     end
+
+   
 end
